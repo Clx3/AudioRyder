81db1e19050a34136f8ee4c488de6b528d155ef4
Teemu Salminen <teemu.salminen@cs.tamk.fi>
2018-02-28 16:10:17 +0200

    Kevyitä muutoksia tyhjään pohjaan ei mitään tärkeetä.

---
+++ core/src/com/mygdx/audioryder/screens/GameScreen.java
@@ -0,0 +1,21 @@
+package com.mygdx.audioryder.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Teemu on 28.2.2018.
+ */
+
+public abstract class GameScreen implements Screen {
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+}
